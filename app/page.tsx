ac020a7dea8bab51d123c6963e0e0a080ce7d017
Rabeet-Ahmer <rabeetahmer9749@gmail.com>
2024-10-20 20:02:25 +0500

    File-Added

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WeatherWidget from '@/components/weather-widget'
+
+const HomePage = () => {
+  return (
+    <div>
+      <WeatherWidget/>
+    </div>
+  )
+}
+
+export default HomePage
